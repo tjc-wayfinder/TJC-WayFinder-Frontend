c021d850e522d3b514ff96f39712114207749954
Suraj Kumar <nehalkumarsuraj@gmail.com>
2023-12-29 23:47:12 +0800

    beautify image display and allow for back routing

--- wayfinder-frontend/src/app/WayFinder/page.tsx
+++ wayfinder-frontend/src/app/WayFinder/page.tsx
@@ -4,6 +4,16 @@ import { useState, useEffect } from "react";
 import { postMessageAndGetImage } from "./getMap";
 import { useAtom } from "jotai";
 import { presentLocation, goLocation } from "@/atoms";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function WayFinder() {
   const [imageUrl, setImageUrl] = useState('');
   const [selectedschLocation] = useAtom(presentLocation);
@@ -26,16 +36,29 @@ const myLoader = ({ src }: { src: string }) => {
 
   return (
     <div className="bg-[#328C2A] flex flex-col h-screen justify-center items-center ">
-      <h1>WayFinder</h1>
-      {imageUrl ? (
-        <Image
-          loader={myLoader}
-          src={imageUrl}
-          alt="Map"
-          width={500}
-          height={500}
-        />
-      ) : null}
+      <Card className="w-auto">
+        <CardHeader>
+          <CardTitle>Find your way</CardTitle>
+          <CardDescription>Wayfind in one click</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {imageUrl ? (
+            <Image
+              loader={myLoader}
+              src={imageUrl}
+              alt="Map"
+              layout="responsive"
+              width={100}
+              height={100}
+            />
+          ) : null}
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button className="px-8 py-4 text-lg">
+            <Link href="/MapInput">Back</Link>
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
